49e73c60cdb276dc3aa21a58c3ed0f6d42ba65b1
Abdullah Alka Kandilli <alkakandilli@gmail.com>
2023-02-27 09:48:45 +0100

    update

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def _max_width_():
         unsafe_allow_html=True,
     )
 
-st.set_page_config(page_icon="✂️", page_title="Logo or Not Logo")
+st.set_page_config(page_icon="✂️", page_title="Question to Image")
 
 
 c2, c3 = st.columns([6, 1])
@@ -30,14 +30,14 @@ with c2:
     c31, c32 = st.columns([12, 2])
     with c31:
         st.caption("")
-        st.title("Logo or Not Logo")
+        st.title("Question to Image")
     with c32:
         st.image(
             "images/logo.png",
             width=200,
         )
 
-images_ = st.file_uploader("Upload PDF", type=["png","jpg","jpeg"], accept_multiple_files=True)
+images_ = st.file_uploader("Upload Images", type=["png","jpg","jpeg"], accept_multiple_files=True)
 # Convert PDF to JPG
 
 df = pd.DataFrame()
@@ -67,7 +67,7 @@ def image_checker(question_):
                 👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
                 """
         )
-
+        st.video('https://youtu.be/5rACpMjHBN0')
         st.stop()
 
     return (df)
@@ -75,13 +75,13 @@ def image_checker(question_):
 form = st.form(key="annotation")
 with form:
     question_ = st.text_input("Enter your query!")
-
+    questions_ = st.text_area
     submitted = st.form_submit_button(label="Submit")
 
 answer_ = pd.DataFrame()
 if submitted:
 
-    answer_ = image_checker(question_)
+    answer_ = image_checker(questions_)
 
 
 c29, c30, c31 = st.columns([1, 1, 2])
@@ -91,6 +91,10 @@ with c29:
     CSVButton = download_button(
         answer_,
         "FlaggedFile.csv",
-        "Download to CSV",
+        "Download tos CSV",
     )
 
+c6, c7, c8 = st.columns([1, 6, 1])
+
+with c7:
+    st.video('https://youtu.be/5rACpMjHBN0')
